d42b016d942abf2cf3cbb886a8a4c7ac6b230459
Ilan Nowak--Glandier <inowak--@2A7.42angouleme.fr>
2025-04-23 10:10:27 +0200

    maj

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:20:44 by inowak--          #+#    #+#             */
-/*   Updated: 2025/04/10 15:41:40 by inowak--         ###   ########.fr       */
+/*   Updated: 2025/04/23 10:10:15 by inowak--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@
 Base * generate(void){
 	std::srand(std::time(0));
 	int grade = std::rand() % 3;
-	if (grade == 0){A* base = new A(); return base;}
-	else if (grade == 1){B* base = new B(); return base;}
-	else if (grade == 2){C* base = new C(); return base;}
+	switch (grade){
+		case 0: {A* base = new A(); return base;}
+		case 1:{B* base = new B(); return base;}
+		case 2:{C* base = new C(); return base;}
+	}
 	return NULL;
 }
 
 void identify(Base* p){
-	if (dynamic_cast<A*>(p)){std::cout << "A" << std::endl;}
-	else if (dynamic_cast<B*>(p)){std::cout << "B" << std::endl;}
-	else if (dynamic_cast<C*>(p)){std::cout << "C" << std::endl;}\
-	else{std::cout << "Unknown" << std::endl;}
+	if (dynamic_cast<A*>(p)) { std::cout << "A" << std::endl; }
+	else if (dynamic_cast<B*>(p)) { std::cout << "B" << std::endl; }
+	else if (dynamic_cast<C*>(p)) { std::cout << "C" << std::endl; }
+	else { std::cout << "Unknown" << std::endl; }
 }
 
 void identify(Base& p){
